a1c7d249b11077c87320b969a175111d1150e395
Shambhavi Singh <shambhavisingh492@gmail.com>
2026-04-16 11:17:59 +0530

    fix cors

--- devtrack-finale2/backend/server.js
+++ devtrack-finale2/backend/server.js
@@ -13,10 +13,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-origin: [
-    "http://localhost:3000",
-    "https://devtrack-final.vercel.app"
-  ],
+origin: "https://devtrack-final.vercel.app",
   credentials: true
 }));
   
